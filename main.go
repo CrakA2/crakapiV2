58a024326448eee0d9c872d109a878cc4ff99431
Adarsh Abhinav <adarsh.citra@gmail.com>
2024-06-02 10:36:00 +0530

    dailies

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 )
 
 func main() {
+	//Handle routes here
 	fmt.Println("Starting server on :http://localhost:3000")
 	router := mux.NewRouter()
 	router.HandleFunc("/v1/account/{name}/{tag}", account.AccountHandler)
